284fb35d28aef2e317a6991b6212384f1f4c9f28
Digambar Shukla <your.email@example.com>
2025-07-21 18:28:45 +0530

    Updated greeting message to 'Hello Ambar Shukla'

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+
 set :bind, '0.0.0.0'
+set :port, 4567
+
+# Allow Render domain access
+set :allowed_hosts, ['k8s-full-project.onrender.com']
+
 get '/' do
-  "Hello Kubernetes!"
+  'Hello Ambar Shukla'
 end
