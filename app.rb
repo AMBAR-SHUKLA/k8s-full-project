80e04f468a94b1439b5733668e30daa1916e01a7
Digambar Shukla <your.email@example.com>
2025-07-21 19:00:32 +0530

    Fix HostAuthorization issue

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require 'sinatra'
 
-# Allow external access
+
 set :bind, '0.0.0.0'
 set :port, 4567
 
-# ✅ Disable Rack::Protection::HostAuthorization completely
+
 set :protection, except: :host_authorization
 
 get '/' do
